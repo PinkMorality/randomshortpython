3cdec6b4a12d9e512f2357d7378575335f49c9e3
PinkMorality <78565440+PinkMorality@users.noreply.github.com>
2021-11-18 15:23:44 +0200

    Add files via upload

---
+++ population redestribute delta.py
@@ -0,0 +1,45 @@
+import csv
+
+total_population = 0
+total_area = 0
+
+with open("C:/Users/Aditya/Desktop/table1.csv", newline="") as csvfile:
+    dict1 = csv.DictReader(csvfile)
+    dict2 = csv.DictReader(csvfile)
+
+    # for row in dict1:
+    #     population = float(row.get("Population"))
+    #     area = float(row.get("Area"))
+    #     density = population/area
+    #     #print(area)
+    #     #print(population)
+    #     population = float(population)
+    #     total_population += population
+    #     total_area += area
+    average_density = 7910157000/122162000
+
+    def change_population(c_pop, c_den, c_area):
+        delta_pop = average_density*c_area - c_pop
+        return delta_pop
+
+
+    print(f"{total_population} people, {total_area} km2")
+    # for row in dict1:
+    #     population = float(row.get("Population"))
+    #     area = float(row.get("Area"))
+    #     density = population/area
+    #     print(area)
+    #     print(population)
+    #     population = float(population)
+    #     total_population += population
+    #     total_area += area
+
+    for row in dict2:
+        population = float(row.get("Population"))
+        area = float(row.get("Area"))
+        density = float(row.get("Density"))
+        change = change_population(population, density, area)
+        population2 = str(population + change)
+        row.update({"Population": population2})
+        row.setdefault("Change",change)
+        print(f"{row}")
