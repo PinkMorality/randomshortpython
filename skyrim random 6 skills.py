67ec782856ce41113e202d080e37c11ade11a82e
PinkMorality <78565440+PinkMorality@users.noreply.github.com>
2021-11-19 16:10:17 +0200

    Add files via upload

--- skyrim random 6 skills.py
+++ skyrim random 6 skills.py
@@ -2,22 +2,36 @@ from random import *
 
 damageskills = ["onehand/twohand", "block", "archery", "destruction"]
 armorskills = ["light armor", "heavy armor", "alteration"]
-supportskills = ["illusion", , "pickpock", "restoration", "conjuration"] #can use sneak depending on build
+supportskills = ["illusion", "restoration", "conjuration"] #can use sneak depending on build
 #enchanting and smithing go to 100 from cheats, alchemy and speech used every run,
 #lockpick also used ##but if chosen, means have to invest perks
+#fuck pickpocket
 while True:
     archoice = [armorskills[randint(0,2)]]
+    dchoice = sample(damageskills, k=2)
+    #d2s2choice = randint(0,0)
+    support_skillsbool = randint(0,1)
+    
+    
+        
+    if damageskills[1] in dchoice and damageskills[0] not in dchoice:
+            z = dchoice.index(damageskills[1])
+            dchoice.pop(z)
 
-    d2s2choice = randint(0,1)
-
-    if d2s2choice == 1:
-        dchoice = sample(damageskills, k=2)
-        schoice = sample(supportskills, k=3)
-    else:
-        dchoice = sample(damageskills, k=3)
+    if support_skillsbool == 1:
         schoice = sample(supportskills, k=2)
+        final = [archoice + schoice + dchoice]
+    else:
+        final = [archoice + dchoice]
+    
+##    if d2s2choice == 1:
+##        dchoice = sample(damageskills, k=2)
+##        schoice = sample(supportskills, k=3)
+##    else:
+##        dchoice = sample(damageskills, k=3)
+##        schoice = sample(supportskills, k=2)
 
-    final = [archoice + schoice + dchoice]
+    
 
 
     input(final)
